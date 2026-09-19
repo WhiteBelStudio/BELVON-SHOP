4c6ef4e1a5f347e6bb5ea066115efabca1c5c472
WhiteBelStudio <nikitinka221@gmail.com>
2026-09-19 14:28:37 +0300

    chore: restore product model

---
+++ lib/models/product.dart
@@ -0,0 +1,9 @@
+class Product {
+  final int id;
+  final String name;
+  final String description;
+  final String category;
+  final double price;
+
+  const Product({required this.id, required this.name, required this.description, required this.category, required this.price});
+}
