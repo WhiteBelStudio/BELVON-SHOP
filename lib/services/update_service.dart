afa02bd13f16008311b0505c70d5da231513aa99
WhiteBelStudio <nikitinka221@gmail.com>
2026-09-19 15:36:28 +0300

    fix: align updater with release 1.2.0

--- lib/services/update_service.dart
+++ lib/services/update_service.dart
@@ -14,7 +14,7 @@ class UpdateInfo {
 }
 
 class UpdateService {
-  static const currentVersion = '1.1.0';
+  static const currentVersion = '1.2.0';
   static const releaseApi =
       'https://api.github.com/repos/WhiteBelStudio/BELVON-SHOP/releases/latest';
 
