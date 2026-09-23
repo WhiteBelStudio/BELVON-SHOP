05905537292651ae14f28463f00aab0ce701dd2c
WhiteBelStudio <nikitinka221@gmail.com>
2026-09-23 23:21:13 +0300

    refactor(ui): add BELVON design system app_dimensions.dart

---
+++ lib/theme/app_dimensions.dart
@@ -0,0 +1,10 @@
+abstract final class AppDimensions {
+  static const radiusSm = 10.0;
+  static const radiusMd = 16.0;
+  static const radiusLg = 22.0;
+  static const radiusXl = 28.0;
+  static const pagePadding = 16.0;
+  static const contentMaxWidth = 1200.0;
+  static const gridGap = 14.0;
+}
+
